b9e0d29d1428f3734f146510ec897343d239dbc6
gilsoncabacazzo <gilsoncaballero@gmail.com>
2026-08-04 20:18:49 -0300

    se agrega permisos iam

---
+++ iam.tf
@@ -0,0 +1,25 @@
+# Rol base que permite a AWS activar la función Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.function_name}-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# Adjuntar política para que la Lambda guarde logs en CloudWatch
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# 🔍 Obtiene dinámicamente los datos de la cuenta de AWS activa
+data "aws_caller_identity" "current" {}
+
+
+
